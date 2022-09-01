d91894ad12d26b06cb22f612d82b52341c431980
¨Paulo <paulohumano@gmail.com>
2022-09-01 17:18:07 -0300

    correccion

--- hola.py
+++ hola.py
@@ -1,7 +1,7 @@
 print('Hola')
-space_odyssey = 2001
-print(space_odyssey)
-liter ='Good night & Good luck'
-print(type(liter))
-lit = True
-print(type(lit))
+SPACE_ODYSEY = 2001
+print(SPACE_ODYSEY )
+LITER ='Good night & Good luck'
+print(type(LITER))
+LIT= True
+print(type(LIT))
